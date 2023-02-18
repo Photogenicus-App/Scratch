9766799a422e70d00f4084bd57a23951e14bc4cf
Michael Garcia <angelo@Michaels-Air.grandenetworks.net>
2023-02-18 15:37:29 -0600

    added home components

---
+++ src/components/Home/Library.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import LibraryInfo from './Library.jsx'
+
+function Library() {
+  const [libraries, setLibraries] = useState([]);
+  
+  useEffect(() => {
+    fetch('')
+      .then(res => res.json())
+      .then(libraryList => {
+        setRecipes(libraryList);
+        console.log('list :', libraryList);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {libraries.map((lib, i) => {
+        <div>
+          <LibraryInfo id={`${i}`}/>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export default Library;
