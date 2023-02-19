0285583418d15cb8a03898ee5a0807da87e85d6f
Mike Dunnmon <99291081+mdunnmon@users.noreply.github.com>
2023-02-18 19:32:18 -0800

    Added create library function. Changed error handling for null return of getLibrary

--- server/controllers/libraryController.js
+++ server/controllers/libraryController.js
@@ -4,24 +4,23 @@ const Library = require('../models/LibraryModel');
 const LibraryController = {
   // Retreive all libraries from the database and send it in the response
   // Sends the found libraries
-
   // Using async/await in try/catch blocks
   async getLibrary(req, res, next) {
     try {
       // Await result of finding library and assign to foundLibrary
       const foundLibrary = await Library.find();
       // If no libraries found return error of no library found
-      if (!foundLibrary) {
+      if (!foundLibrary.length) {
         return next({
-          log: 'Library not found',
+          log: 'No libraries found',
           status: 400,
-          message: { err: 'Cannot find library' },
+          message: { err: 'No libraries returned from database' },
         });
       }
       // Assigns found library to our locals object to return
       res.locals.library = foundLibrary;
       return next();
-      // Catch block to return any other erros other than a null return of finding libraries
+      // Catch block to return any other errors other than a null return of finding libraries
     } catch (error) {
       return next({
         log: 'Error within getLibrary',
@@ -30,7 +29,38 @@ const LibraryController = {
       });
     }
   },
-
+  // Create new library
+  // Using similar async/await and try catch block format as above
+  async newLibrary(req, res, next) {
+    try {
+      // Deconstruct request body to sanitize data
+      const { title, description, studyMaterial } = req.body;
+      // Create new library with our deconstructed request body
+      const newLibrary = await Library.create({
+        title,
+        description,
+        studyMaterial,
+      });
+      // Error handler for error within creating new library
+      if (!newLibrary) {
+        return next({
+          log: 'No library created',
+          status: 400,
+          message: { err: 'No new library created' },
+        });
+      }
+      // Assign new library for return
+      res.locals.newLibrary = newLibrary;
+      return next();
+      // Catch block for other errors within middleware
+    } catch (error) {
+      return next({
+        log: 'Error within newLibrary',
+        status: 400,
+        message: { err: 'Error creating Library' },
+      });
+    }
+  },
   ///other middleware here
 };
 
--- server/routes/libraryRouter.js
+++ server/routes/libraryRouter.js
@@ -7,10 +7,16 @@ const router = express.Router();
 
 //// Specific routing goes here for .get, .post, etc....
 
-// Router for get request to library
+// Router for get request to library to receive all libraries
 router.get('/library', LibraryController.getLibrary, (req, res) =>
-  // Sends 200 status and the entire library back to the client as aa payload
+  // Sends 200 status and the entire library back to the client
   res.status(200).json(res.locals.library)
+);
+
+// Router for post to library creating new library
+router.post('/library', LibraryController.newLibrary, (req, res) =>
+  // Sends 200 status and the created library back to the client
+  res.status(200).json(res.locals.newLibrary)
 );
 
 // Export as router
